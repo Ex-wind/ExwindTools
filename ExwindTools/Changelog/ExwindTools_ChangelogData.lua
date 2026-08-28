f6232338f447ebfde9522a6ab050e53066684d17
EXWIND <exwind081697@gmail.com>
2026-08-29 03:29:43 +0800

    chore: 更新更新日志至 v26.8.28.2349，添加大秘境分数线和周围怪物DEBUFF内容

--- ExwindTools/Changelog/ExwindTools_ChangelogData.lua
+++ ExwindTools/Changelog/ExwindTools_ChangelogData.lua
@@ -1,20 +1,23 @@
 -- ExwindTools 的游戏内更新日志正文。由发布打包器写入；Core 只提供共享查看窗口。
 ExwindTools_ChangelogData = {
     changelog = {
-        version = "v26.8.24.2151",
-        title = "v26.8.24.2151 更新日志",
-        publishedAt = "2026-08-24 21:51",
+        version = "v26.8.28.2349",
+        title = "v26.8.28.2349 更新日志",
+        publishedAt = "2026-08-28 23:49",
         fontSize = 14,
         content = [[
+@H1@ v26.8.28.2349
+
+@CN@ @H2@ 大秘境分数线
+@CN@ - 更新S2赛季分数线
+
+@CN@ @H2@ 周围怪物DEBUFF
+@CN@ - 新增按天赋载入
+
 @H1@ v26.8.24.2151
 
-[CN_0]
-@H2@ 0824测试
-- TEST
-[CN_1]
-[EN_0]
-
-[EN_1]
+@CN@ @H2@ 0824测试
+@CN@ - TEST
         ]],
     },
 }
